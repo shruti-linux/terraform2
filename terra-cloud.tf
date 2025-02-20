3ad4876ab80b9441595be84cb15ebdaffa0de3d3
shruti-linux <131751004+shruti-linux@users.noreply.github.com>
2025-02-20 20:50:11 +0400

    Update terra-cloud.tf

--- terra-cloud.tf
+++ terra-cloud.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
   subscription_id = "79c65e59-aadb-45ab-9a43-3e10d9e5225e"
 }
 
-resource "azurerm_resource_group" "example1472398" {
-  name     = "inarasaxena"
+resource "azurerm_resource_group" "example147239238" {
+  name     = "inarasaxena34"
   location = "westus"
 }
